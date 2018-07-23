5da707b795b068431d07e39fe1dfc3047dfc761a
jtconnors <github@jtconnors.com>
2018-07-23 16:38:49 -0400

    makeAllScripts.ps1: Edit due to Windows 10 dependency service name change
    Had to remove dependency to WebLogic NodeManager because its name is different depending on the version of Windows.
    Windows 7: Oracle Weblogic base_domain NodeManager (D_Oracle_MIDDLE~1_ORACLE~1_wlserver)
    Windows 10: Oracle Weblogic base_domain NodeManager (D_Oracle_Middleware_Oracle_Home_wlserver)

--- scripts/makeAllScripts.ps1
+++ scripts/makeAllScripts.ps1
@@ -16,5 +16,6 @@ $BAR_WL_HOME = $BAR_WL_HOME -replace '\\','_'
 cd $MW_HOME\wlserver\server\bin
 cat .\installSvc.cmd | %{$_ -replace "-svcdescription","-delay:15000 -svcdescription"} | Out-File -Encoding ASCII installWebLogicSvc.cmd
 cat .\installNodeMgrSvc.cmd.wls-orig | %{$_ -replace "-svcdescription",'-depend:"wlsvc base_domain_AdminServer" -svcdescription'} | Out-File -Encoding ASCII installNodeMgrSvc.cmd
-$DEPEND="-depend:`"wlsvc base_domain_AdminServer`",`"Oracle Weblogic base_domain NodeManager " + "(" + $BAR_WL_HOME + "_MIDDLE~1_ORACLE~1_wlserver)`""
+#$DEPEND="-depend:`"wlsvc base_domain_AdminServer`",`"Oracle Weblogic base_domain NodeManager " + "(" + $BAR_WL_HOME + "_MIDDLE~1_ORACLE~1_wlserver)`""
+$DEPEND="-depend:`"wlsvc base_domain_AdminServer`""
 cat .\installSvc.cmd | %{$_ -replace "-svcdescription","$DEPEND -svcdescription"} | Out-File -Encoding ASCII installAMCServerSvc.cmd
